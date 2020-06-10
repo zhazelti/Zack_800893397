b075f9a643feaceb9d41b85c4dc9f706fa5690cc
zhazelti <zhazelti@uncc.edu>
2020-06-10 18:39:39 -0400

    added javascript file and updated html
    added a javascript file to support changing the size of text and the color of the title image. updated the HTML to make use of the javascript file, and add needed inputs.

---
+++ admin/ProjectJavascript.js
@@ -0,0 +1,30 @@
+function smallerText(){
+	document.getElementById("bodyID").style.fontSize =  "smaller";
+	}
+	function largerText(){
+	document.getElementById("bodyID").style.fontSize =  "larger";
+	}
+	function changeBlue() {
+	document.getElementById("backImage").src = "admin/tournamentCenterBlue.png";
+	}
+	function changeRed() {
+	document.getElementById("backImage").src = "admin/tournamentCenterRed.png";
+	}
+	function changeGreen() {
+	document.getElementById("backImage").src = "admin/tournamentCenterGreen.png";
+	}
+	function changeOriginal() {
+	document.getElementById("backImage").src = "admin/tournamentCenter.png";
+	}
+	function changeBlueAlt() {
+	document.getElementById("backImage").src = "tournamentCenterBlue.png";
+	}
+	function changeRedAlt() {
+	document.getElementById("backImage").src = "tournamentCenterRed.png";
+	}
+	function changeGreenAlt() {
+	document.getElementById("backImage").src = "tournamentCenterGreen.png";
+	}
+	function changeOriginalAlt() {
+	document.getElementById("backImage").src = "tournamentCenter.png";
+	}
